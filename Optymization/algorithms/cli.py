472bb423de35a2d34a3fc1c3afd8aaad633456df
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2018-04-03 21:11:20 +0200

    Patched part of cli.py

--- Optymization/algorithms/cli.py
+++ Optymization/algorithms/cli.py
@@ -1,113 +1,154 @@
 # commands for algo
-ALGO_TABU = '0'
-ALGO_SA   = '1'
+ALGO_TABU = 0
+ALGO_SA   = 1
 
 # commands for options
-OPT_QUIT  = '0'
-OPT_RUN   = '1'
-OPT_CFG   = '2'
-OPT_ALGO  = '3'
-OPT_DATA  = '4'
-OPT_PROC  = '5'
+OPT_QUIT  = 0
+OPT_RUN   = 1
+OPT_CFG   = 2
+OPT_ALGO  = 3
+OPT_DATA  = 4
+OPT_PROC  = 5
 
-OPT_MAX   = '5'
+OPT_MAX   = 5
 
 # list of all commands
 COMMANDS = [
-    ALGO_TABU,
-    ALGO_SA,
-    OPT_QUIT,
-    OPT_RUN,
-    OPT_CFG,
     OPT_ALGO,
+    OPT_CFG ,
     OPT_DATA,
-    OPT_PROC
+    OPT_PROC,
+    OPT_QUIT,
+    OPT_RUN ,
+    ALGO_SA ,
+    ALGO_TABU
 ]
 
+# reusable huge strings for display
+USR_CHOICE = """
+=== ALGORITHM CHOICE
+** Algorithm choice:
+** """ + str(ALGO_TABU) + """ / Tabu search
+** """ + str(ALGO_SA)   + """ / Simulated annealing
+"""
 USR_INPUT = "solver > "
-USR_PARAMETERS = "== Parameters setting"
-USR_NBPROCESS = "** Number of processors:\n"
-USR_TASKS = "** Tasks durations (separated by ' '):\n** example: '5 7 6 1 2 4'\n"
-USR_CHOICE = "** Algorithm choice:\n** 0/Tabu search\n** 1/ Simulated annealing"
+USR_INTRO = """
+===
+** Task repartition solver
+**
+** Written in Python by:
+**      - Ewan Le Goallec [github.com/eLeGoalled] 
+**      - Pierre Bouillon [pierrebouillon.tech/]
+**
+** Java version also available at [github.com/pBouillon/L3_Optimization]
+** Copyright (c) 2018 Pierre Bouillon, Ewan Le Goallec
+** MIT Licence
+===
+"""
+USR_MENU = """
+=== MENU
+** """ + str(OPT_RUN)  + """ / Run with current parameters
+** """ + str(OPT_CFG)  + """ / See current parameters
+** """ + str(OPT_ALGO) + """ / Change algorithm
+** """ + str(OPT_DATA) + """ / Change data
+** """ + str(OPT_PROC) + """ / Change number of processes
+** """ + str(OPT_QUIT) + """ / Exit
+"""
 
-class Cli :
 
-    """Optimization solver"""
-    """int nbProc"""
-    """int[] data"""
-    """OptimizationAlgorithm algo"""
-    """int tabuLimit"""
-    """int initTemp"""
-
+class CLI:
+    """ Cli
+    Class grouping several static methods to build the CLI
+    """
 
     @staticmethod
-    def __getUsrInput() -> str :
+    def __get_usr_input() -> int :
+        """
+        Get the user input and ensure that it's an int
+        :return: user input as integer
+        """
+        usr_inp = input(USR_INPUT)
+        while not usr_inp.isdigit():
+            print ('~~ WRONG TYPE, expected an integer')
+        return int(usr_inp)
+
+    @staticmethod
+    def __show_tasks_repartition():
+        """
+
+        :return:
+        """
+        print (USR_INTRO)
+
+    @staticmethod
+    def __specify_processes_number() -> str:
+        """
+
+        :return:
+        """
+        print ('** Please specify how many processes you have')
         return input(USR_INPUT)
 
-
     @staticmethod
-    def __showTaskRepartition():
-        print(
-        "==\n"
-        "** Task repartition solver\n"
-        "**\n"
-        "** Written in Java by\n" 
-        "**     - Ewan Le Goallec [github.com/eLeGoalled]\n"
-        "**     - Pierre Bouillon [pierrebouillon.tech/]\n"
-        "**\n"
-        "** Copyright (c) 2018 Pierre Bouillon, Ewan Le Goallec \n"
-        "** MIT Licence \n"
-        "==\n"
+    def __specify_data() -> list:
+        """
+
+        :return:
+        """
+        print (
+            '** Tasks durations (separated by ' '; ex: 5 7 6 1 2 4)'
         )
-
-    @staticmethod
-    def __specifyNumberProcess() -> str:
-        print(USR_NBPROCESS)
-        return input(USR_INPUT)
-
-    @staticmethod
-    def __specifyData() -> list:
-        print(USR_TASKS)
         data = input(USR_INPUT)
         return data.split()
 
     @staticmethod
-    def __chooseAlgorithm():
-        print(USR_CHOICE)
-        choiceAlgo = input(USR_INPUT)
-        if choiceAlgo == ALGO_TABU :
-            Cli.__showMenu()
+    def __choose_algorithm():
+        """
+
+        :return:
+        """
+        print (USR_CHOICE)
 
     @staticmethod
-    def __showMenu():
-        print(
-            "== MAIN\n"
-            "MENU\n"
-            "** 1 / Run with current parameters\n"
-            "** 2 / See current parameters\n"
-            "** 3 / Change algorithm\n"
-            "** 4 / Change data\n"
-            "** 5 / Change number of processes\n"
-            "** 0 / Leave CLI\n"
-        )
-        choice = input(USR_INPUT)
+    def __show_menu():
+        """
+
+        :return:
+        """
+        print (USR_MENU)
+        choice = CLI.__get_usr_input()
+
         if choice == OPT_RUN:
-            Cli.__run()
-        if choice == OPT_CFG:
-            Cli.__printData()
-        if choice == OPT_ALGO:
-            Cli.__chooseAlgorithm()
-        if choice == OPT_DATA:
-            Cli.__specifyData()
-        if choice == OPT_PROC:
-            Cli.__specifyNumberProcess()
-        if choice == OPT_QUIT:
-            """leave cli"""
+            CLI.__run()
+
+        elif choice == OPT_CFG:
+            CLI.__print_data()
+
+        elif choice == OPT_ALGO:
+            CLI.__choose_algorithm()
+
+        elif choice == OPT_DATA:
+            CLI.__specify_data()
+
+        elif choice == OPT_PROC:
+            CLI.__specify_processes_number()
+
+        elif choice == OPT_QUIT:
+            print ('=== CLOSING CLI')
+            exit (0)
 
     @staticmethod
     def __run():
+        """
+
+        :return:
+        """
         print("run")
 
     @staticmethod
-    def __printData():
+    def __print_data():
+        """
+
+        :return:
+        """
         print("Data")
